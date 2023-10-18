70a245cbca85efce47c87d75d353c1ce81d336da
PhiHung1503 <phihung15032001@gmail.com>
2023-10-18 17:21:31 +0700

    create api

---
+++ client/src/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./home.scss";
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <AcUnitIcon/>
+    </div>
+  )
+}
+
+export default Home
